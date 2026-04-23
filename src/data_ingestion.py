11f9f17dccdd29b1add44932f6b81d4440d6ae1c
KhushiAggarwal-Brimo <insomnia.test.200@gmail.com>
2026-04-24 01:53:10 +0530

    pipelein automated with parameters using dvc repro

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -87,9 +87,9 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 
 def main():
     try:
-        # params = load_params(params_path='params.yaml')
-        # test_size = params['data_ingestion']['test_size']
-        test_size = 0.2
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
+        # test_size = 0.2
         data_path = 'https://raw.githubusercontent.com/kumargangesh/Datasets/refs/heads/main/train_and_test2.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
